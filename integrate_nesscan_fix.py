4885ad0abda3f1c3589154f781cb2a5cc6f2bbbd
ncail <ncail@islander.tamucc.edu>
2025-02-05 17:14:00 -0600

    removed generated_files/ from tracking and added integrate_nesscan_fix.py to repo.

---
+++ integrate_nesscan_fix.py
@@ -0,0 +1,97 @@
+# Imports.
+import glob
+import pandas as pd
+import os
+import helpers
+
+# ***************************************************************************
+# *************************** PROGRAM START *********************************
+# ***************************************************************************
+
+# Specify station (as given in data/lighthouse directory).
+station = "rockport"
+
+# Specify write path.
+# duplicates_path = f'generated_files/nesscan-fixed_duplicates'
+# if not os.path.exists(duplicates_path):
+#     os.makedirs(duplicates_path)
+
+fixed_lh_file_path = f'data/lighthouse/{station}_nesscan_fixed'
+if not os.path.exists(fixed_lh_file_path):
+    os.makedirs(fixed_lh_file_path)
+
+# Get lighthouse and nesscan_fixed data files.
+lighthouse_files_path = f"data/lighthouse/{station}/preprocessed"
+nesscan_files_path = f"data/lighthouse/nesscan_fix_full_history_12172024/{station}/{station}/preprocessed"
+lighthouse_files = glob.glob(f"{lighthouse_files_path}/*.csv")
+nesscan_files = glob.glob(f"{nesscan_files_path}/*.csv")
+
+# Give everything the same header, so we can use pd.update() to merge the values.
+cols = ['dt', 'wl']
+
+# Put into dataframes dictionaries by year.
+lh_dfs = {}
+lh_df_arr = []
+for file in lighthouse_files:
+    df = pd.read_csv(file, parse_dates=[0])
+
+    # Rename columns.
+    df_cols = df.columns.tolist()
+    df_cols[0] = cols[0]
+    df_cols[1] = cols[1]
+    df.columns = df_cols
+
+    # Drop other columns.
+    for col in df.columns:
+        if col not in cols:
+            df.drop(columns=col, inplace=True)
+
+    lh_df_arr.append(df)
+# End for.
+lh_dfs = helpers.get_df_dictionary(lh_df_arr, 0)
+
+nesscan_dfs = {}
+nes_df_arr = []
+for file in nesscan_files:
+    df = pd.read_csv(file, parse_dates=[0])
+
+    # Add label to the columns.
+    df.columns = cols
+
+    nes_df_arr.append(df)
+# End for.
+nesscan_dfs = helpers.get_df_dictionary(nes_df_arr, 0)
+
+# Get common years.
+common_years = set(lh_dfs.keys()) & set(nesscan_dfs.keys())
+
+# Loop over common years.
+for year in common_years:
+
+    lh_df = lh_dfs[year]
+    nesscan_df = nesscan_dfs[year]
+
+    # Find and write duplicates.
+    duplicates = nesscan_df[nesscan_df.duplicated(subset=cols[0], keep=False)]
+
+    # Print the duplicates, write to file. Update: in the nesscan-fixed data, the files highly overlap the dates
+    # printed to the output file (e.g. 1993 output contains days from 1992, also present in the 1992 file) so just
+    # remove duplicates, it is impossible to tell what is a systemic duplicate if the output is like this.
+    # print(duplicates)
+    # write_header = not Path(f"{duplicates_path}/{station}.csv").exists()
+    # duplicates.to_csv(f"{duplicates_path}/{station}.csv", index=False, header=write_header, mode='a')
+
+    # Ensure there are no duplicate datetime values in nesscan_df. Keep the first instance.
+    nesscan_df = nesscan_df.drop_duplicates(subset=cols[0], keep='first')
+
+    lh_df.set_index('dt', inplace=True)
+    nesscan_df.set_index('dt', inplace=True)
+
+    lh_df.update(nesscan_df)  # Updates df1 values with df2 where dt matches
+
+    lh_df.reset_index(inplace=True)
+
+    lh_df.to_csv(f"{fixed_lh_file_path}/{year}.csv", index=False)
+# ***************************************************************************
+# *************************** PROGRAM END ***********************************
+# ***************************************************************************
